bfa02ef002e5328fb4ad595456c08ec517750568
Daniel Reed <daniel.reed998@gmail.com>
2017-11-06 17:16:06 -0600

    small change

--- app.js
+++ app.js
@@ -2,29 +2,22 @@ const express = require('express');
 const app = express();
 const PORT = 3000
 app.listen(PORT, () => {
-  console.log('listening on port 3000')
+    console.log('listening on port 3000')
 })
 
 app.use((req, res, next) => {
-  console.log(req.method + ' ' + req.path)
-  //console.log(res.method + ' ' + res.path)
-  //console.log(res)
-  //console.log(res.method + ' ' + res.path)
-  next();
+    console.log(req.method + ' ' + req.path)
+    next();
 })
 app.get('/', (req, res, next) => {
-  res.send('hiiiii there!!!!')
-  next();
+    res.send('hiiiii there!!!!')
+    next();
 });
-app.post('/modernism', (req, res, next)=> {
-  res.send('post request to modernism')
+app.post('/modernism', (req, res, next) => {
+    res.send('post request to modernism')
 })
 
 // app.get('/is-anybody-in-there', (req, res, next) => {
 //   res.send('neeeeeewsssss!!!!')
 //   next()
 // });
-
-
-
-
