09dd88c81e2933717f69b3f54d33ee761a96ea43
Courtney Napleton <courtney.napleton@gmail.com>
2017-11-07 09:45:26 -0600

    template is populated

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const nunjucks  = require('nunjucks')
+const volleyball  = require('volleyball')
+const chalk  = require('chalk')
 const app = express();
 const PORT = 3000
 app.listen(PORT, () => {
     console.log('listening on port 3000')
 })
-
+app.use(volleyball)
 app.use((req, res, next) => {
     console.log(req.method + ' ' + req.path)
     next();
@@ -16,6 +19,40 @@ app.get('/', (req, res, next) => {
 app.post('/modernism', (req, res, next) => {
     res.send('post request to modernism')
 })
+var locals = {
+    title: 'Lord of the Nunjucks',
+    people: [
+        { name: 'Gandalf'},
+        { name: 'Frodo' },
+        { name: 'Hermione'}
+    ]
+};
+
+app.engine('html', nunjucks.render);
+app.set('view engine', 'html');
+nunjucks.configure('views', {noCache: true})
+// nunjucks.render('index.html', locals, function(err, res){
+//     if (err) throw err;
+//     console.log(locals)
+// });
+
+app.get('/views/index.html', (req, res) => {
+    res.render('index', locals);
+})
+
+
+// nunjucks.configure('/', {
+//   autoescape: true,
+//   express: app
+// })
+// app.get('/', function(req, res) {
+//   res.render('./views/index.html');
+// })
+// nunjucks.renderString('Hello {{ username }}', { username: 'James' });
+// var people = {
+//   name:'Frodo'
+// }
+//nunjucks.render('/index.html', people)
 
 // app.get('/is-anybody-in-there', (req, res, next) => {
 //   res.send('neeeeeewsssss!!!!')
